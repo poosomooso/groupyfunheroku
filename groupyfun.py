343ce7f314176e9f5c2219f0b7c13026819afad6
Serena Chen <serenazychen@gmail.com>
2016-03-06 13:38:35 -0500

    more things?

--- groupyfun.py
+++ groupyfun.py
@@ -1,7 +1,15 @@
-import groupy
+#import groupy
+import requests
 
-groups = groupy.Group.list()
-print(groups)
-bots = groupy.Bot.list()[0]
-print(bots)
-bots.post('snails are a lie')
+# groups = groupy.Group.list()
+# print(groups)
+# bots = groupy.Bot.list()[0]
+# print(bots)
+# bots.post('snails are a lie')
+
+
+
+body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'is it ok if i might spam', 'token':'INMXfur7OSiUV9JTbHhRBb045LHRLSkU23HQoU5D'}
+
+# Make the POST request here, passing body as the data:
+response = requests.post('https://api.groupme.com/v3/bots/post/', data=body)
