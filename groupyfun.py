f41ab574128d5eed189449790e4c79b1a204464c
Serena Chen <serenazychen@gmail.com>
2016-03-06 14:38:53 -0500

    new changes

--- groupyfun.py
+++ groupyfun.py
@@ -9,7 +9,7 @@ import requests
 
 
 
-body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'is it ok if i might spam', 'token':'INMXfur7OSiUV9JTbHhRBb045LHRLSkU23HQoU5D'}
+body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'daily reminder that you\'re gross', 'token':'INMXfur7OSiUV9JTbHhRBb045LHRLSkU23HQoU5D'}
 
 # Make the POST request here, passing body as the data:
 response = requests.post('https://api.groupme.com/v3/bots/post/', data=body)
