ca13c537d9d9e0f80a58446ef0943e55dd7e169c
Serena Chen <serenazychen@gmail.com>
2016-03-15 18:25:10 -0400

    with random wiki article

--- groupyfun.py
+++ groupyfun.py
@@ -1,5 +1,5 @@
 #import groupy
-import requests
+import requests, urllib2
 
 # groups = groupy.Group.list()
 # print(groups)
@@ -7,9 +7,11 @@ import requests
 # print(bots)
 # bots.post('snails are a lie')
 
+link = 'https://en.wikipedia.org/wiki/Special:Random'
 
+page = urllib2.urlopen(link).geturl()
 
-body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'daily reminder that you\'re gross', 'token':'INMXfur7OSiUV9JTbHhRBb045LHRLSkU23HQoU5D'}
+body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'Daily Reminder that you\'re gross: '+link, 'token':'INMXfur7OSiUV9JTbHhRBb045LHRLSkU23HQoU5D'}
 
 # Make the POST request here, passing body as the data:
 response = requests.post('https://api.groupme.com/v3/bots/post/', data=body)
