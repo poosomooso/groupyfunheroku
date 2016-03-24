a565001011d017d2dd1c86424197f5fa33e9e833
Serena Chen <serenazychen@gmail.com>
2016-03-24 10:58:00 -0400

    cleaning up the repo

---
+++ README.md
@@ -0,0 +1,5 @@
+#Groupy Fun
+
+Having some fun with bots in GroupMe.
+
+Includes a script to generate a random Wikipedia page and sends it to a group in GroupMe via one of my bots. This code utilizes Heroku scheduler to run the script every day.
--- groupyfun.py
+++ groupyfun.py
@@ -1,6 +1,9 @@
 #import groupy
 import requests, urllib2
 
+tokenfile = open('token.txt')
+TOKEN = tokenfile.read()
+tokenfile.close()
 # groups = groupy.Group.list()
 # print(groups)
 # bots = groupy.Bot.list()[0]
@@ -11,7 +14,7 @@ link = 'https://en.wikipedia.org/wiki/Special:Random'
 
 page = urllib2.urlopen(link).geturl()
 
-body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'Daily Reminder that you\'re gross: '+page, 'token':'INMXfur7OSiUV9JTbHhRBb045LHRLSkU23HQoU5D'}
+body = {'bot_id': '335cd8afefe1251ead5f495127', 'text': 'Daily Reminder that you\'re gross: '+page, 'token':TOKEN}
 
 # Make the POST request here, passing body as the data:
 response = requests.post('https://api.groupme.com/v3/bots/post/', data=body)
